b3453cb5d2b56da4129f728d3fa49d4ac9323c48
Shekhu26001 <shekhar99779977@gmail.com>
2024-12-22 12:07:42 +0530

    login

---
+++ Routes/Products.js
@@ -0,0 +1,29 @@
+/*const ensureAuthenticated = require('../Middilewere/Auth');
+const Products=require("../Models/ProductS")
+const FoodCategory =require("../Models/FoodCategory")
+const router = require('express').Router();
+router.post('/product', async (req, res)=>{
+    const {name, CategoryName,img,price, description} =req.body;
+    const Item = new Products ({name, CategoryName,img,price, description}) 
+    await Item.save()
+    res.status(200).json({message:"Item craete succesfully "})
+})
+router.post('/foodCategory', async (req, res)=>{
+    const { CategoryName} =req.body;
+    const FItem = new FoodCategory ({ CategoryName}) 
+    await FItem.save()
+    res.status(200).json({message:"Food Category craete succesfully "})
+})
+
+router.get('/foodscategory', async (req, res) => {
+    const Fitems = await FoodCategory.find()
+    res.status(200).json(Fitems)
+   
+});
+router.get('/foodsItem', async (req, res) => {
+    const items = await Products.find()
+    res.status(200).json(items)
+   
+});
+
+module.exports = router;*/
---
+++ index.js
@@ -0,0 +1,28 @@
+const express =require("express")
+const app = express();
+const dotenv= require("dotenv");
+const bodyParser = require("body-parser");
+const cors= require("cors")
+const AuthRouter =require("./Routes/Authrouter")
+const ProductRouter =require("./Routes/Products")
+const corsConfig ={
+    origin : "*",
+    Credential: true,
+    methods: ["GET","POST","PUT","PATCH",]
+
+}
+dotenv.config();
+require("./Models/db")
+app.get("/d",(req,res)=>{
+    res.send("ram")
+})
+app.use(bodyParser.json())
+
+app.options(" ",cors(corsConfig))
+app.use(cors(corsConfig))
+const PORT =process.env.PORT
+app.use('/auth', AuthRouter);
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT} `)
+})
